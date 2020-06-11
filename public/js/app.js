72f65e78ef91a1ffb96b4a7d1518b1b9987c47db
Femi <tok2ogunz@gmail.com>
2020-06-11 12:43:30 +0100

    Removed unnecessary fetch api call

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,4 @@
-// fetch("http://localhost:3000/weather?address=philadelphia")
-//   .then(res => res.json())
-//   .then(data => {
-//     if (data.error) {
-//       console.log(data.error);
-//     } else {
-//       console.log(data);
-//     }
-//   });
+
 
 const form = document.querySelector("form");
 const input = document.querySelector("input");
